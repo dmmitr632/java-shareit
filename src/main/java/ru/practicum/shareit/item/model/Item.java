ca6584edb221223cba545690068405f824207329
dmmitr632 <df6tb43@yandex.ru>
2023-01-25 20:36:29 +0300

    move model files to model packages

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,29 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class Item {
+    @EqualsAndHashCode.Exclude
+    private int id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+    private int owner;
+    private Integer request;
 }
