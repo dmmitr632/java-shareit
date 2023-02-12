007b2b1e32786d4792996664eb8250f922e53e76
dmmitr632 <df6tb43@yandex.ru>
2023-02-12 15:52:22 +0300

    fix: fix validateBooking comparsion bug

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -135,7 +135,7 @@ public class BookingServiceImpl implements BookingService {
 
     public void validateBooking(Booking booking) {
         LocalDateTime currentTime = LocalDateTime.now();
-        if (booking.getBooker().getId() == booking.getItem().getOwner().getId()) {
+        if (Objects.equals(booking.getBooker().getId(), booking.getItem().getOwner().getId())) {
             throw new NotFoundException("Booker and owner cannot be the same person");
         }
         if (booking.getEnd().isBefore(currentTime) || booking.getStart().isBefore(currentTime)) {
