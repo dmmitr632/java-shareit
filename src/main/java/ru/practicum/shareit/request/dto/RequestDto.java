5ad27aa7394e5c7f8ed441640aa7b62930732ae5
dmmitr632 <df6tb43@yandex.ru>
2023-02-15 00:00:58 +0300

    feat: add RequestControllerTest and equals/hashCode methods to RequestDto for Mockito.verify()

--- src/main/java/ru/practicum/shareit/request/dto/RequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/RequestDto.java
@@ -6,6 +6,7 @@ import ru.practicum.shareit.item.dto.ItemShortDto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Set;
 
 @Builder
@@ -19,4 +20,28 @@ public class RequestDto {
     @NotBlank @NotNull String description;
     LocalDateTime created;
     Set<ItemShortDto> items;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof RequestDto)) {
+            return false;
+        }
+
+        RequestDto that = (RequestDto) o;
+
+        if (!Objects.equals(id, that.id)) {
+            return false;
+        }
+        return Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
 }
