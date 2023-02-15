08bee1c7c7cf13dd7a4bbf37fcbe1593c381a246
dmmitr632 <df6tb43@yandex.ru>
2023-02-15 11:09:19 +0300

    feat: add tests for BookingShort and ItemShortDto to ItemDtoTest

--- src/test/java/ru/practicum/shareit/item/ItemDtoTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemDtoTest.java
@@ -5,28 +5,75 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
+import ru.practicum.shareit.item.dto.BookingShort;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemShortDto;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
 public class ItemDtoTest {
     @Autowired
-    private JacksonTester<ItemDto> jacksonTester;
+    private JacksonTester<Object> jacksonTester;
+    private final LocalDateTime week = LocalDateTime.of(2024, 2, 14, 12, 0, 15);
+    private final LocalDateTime nextWeek = week.plusWeeks(1);
 
     @Test
     void itemDto() throws Exception {
         ItemDto itemDto = ItemDto.builder()
                 .id(1)
                 .name("item")
-                .available(false)
+                .available(true)
                 .description("description")
                 .build();
 
-        JsonContent<ItemDto> jsonContent = jacksonTester.write(itemDto);
+        JsonContent<Object> jsonContent = jacksonTester.write(itemDto);
         assertThat(jsonContent).extractingJsonPathNumberValue("$.id").isEqualTo(1);
         assertThat(jsonContent).extractingJsonPathStringValue("$.name").isEqualTo("item");
         assertThat(jsonContent).extractingJsonPathStringValue("$.description").isEqualTo("description");
-        assertThat(jsonContent).extractingJsonPathBooleanValue("$.available").isFalse();
+        assertThat(jsonContent).extractingJsonPathBooleanValue("$.available").isTrue();
     }
+
+    @Test
+    void BookingShort() throws Exception {
+        BookingShort bookingShort = BookingShort.builder()
+                .id(1)
+                .start(week)
+                .end(nextWeek)
+                .bookerId(1)
+                .build();
+
+        System.out.println(bookingShort);
+
+        JsonContent<Object> jsonContent = jacksonTester.write(bookingShort);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(jsonContent).extractingJsonPathStringValue("$.start")
+                .isEqualTo(LocalDateTime.of(2024, 2, 14, 12, 0, 15).toString());
+        assertThat(jsonContent).extractingJsonPathStringValue("$.end")
+                .isEqualTo(LocalDateTime.of(2024, 2, 21, 12, 0, 15).toString());
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.bookerId").isEqualTo(1);
+    }
+
+    @Test
+    void ItemShortDto() throws Exception {
+        ItemShortDto itemShortDto = ItemShortDto.builder()
+                .id(1)
+                .name("item")
+                .description("description")
+                .requestId(1)
+                .available(false)
+                .ownerId(123)
+                .build();
+
+        JsonContent<Object> jsonContent = jacksonTester.write(itemShortDto);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(jsonContent).extractingJsonPathStringValue("$.name").isEqualTo("item");
+        assertThat(jsonContent).extractingJsonPathStringValue("$.description").isEqualTo("description");
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.requestId").isEqualTo(1);
+        assertThat(jsonContent).extractingJsonPathBooleanValue("$.available").isFalse();
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.ownerId").isEqualTo(123);
+    }
+
 }
